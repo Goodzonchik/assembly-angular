4e2754ca213c3f24e745f821366060da3c3b9b4d
Goodzonchik <goodzonchik@rambler.ru>
2021-10-11 08:04:15 +0500

    add first assembly code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'assembly-angular';
+  assebmly: string = '';
+
+  ngOnInit(): void {
+    let importObject: any;
+    WebAssembly.instantiateStreaming(fetch('assets/assembly/optimized.wasm'), importObject).then((results) => {
+      this.assebmly = (results.instance.exports as any).add(2, 3);
+    });
+  }
 }
